ffbdcf0f53f3144df0d165be15e665d750f2616a
elango-ux <76845186+elango-ux@users.noreply.github.com>
2021-03-03 09:50:19 +0530

    Update classmultipleof10.py
    sample program

--- python shiva project/classmultipleof10.py
+++ python shiva project/classmultipleof10.py
@@ -1,24 +1,33 @@
 class Mynum:
-    #constructor
-    def __init__(self, x ):
+    # constructor it execute start along with point object
+    def __init__(self, x):
         self.x = x
-            
-    def __repr__(self):  
-        return " % s"% (self.x) 
-    
-    list = []
-    def multiple(self):
-        list.append(self)
-        filtered = [item for item in list if item.x % 10 == 0]
         
-        print(filtered)
+        
+        
+        
+    # convert point object value to human readable form
+    def __str__(self):
+        return f"{self.x}"
+    # def __iter__(self):
+  
         
 
-list =[]
-for i in  range(1, 201):
-    point = Mynum(i * 5)
-    point.multiple()
-    
+list = []
+for i in range(1, 11):
+   point = Mynum(i * 5)
+   list.append(point)
+iter_list = iter(list)
+while True: 
+    try: 
+         k = iter_list.__next__()  
+          
+         if k % 10 == 0:
+             print(k)
+    except: 
+        break
+
+
     
 
 
